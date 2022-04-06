c0d4332a94e9ac09eefcfec1e151c04af193f1e5
denakwasi <denakwasi@gmail.com>
2022-04-06 20:33:12 +0000

    Changes made for Heroku deployment

--- users/settings.py
+++ users/settings.py
@@ -188,7 +188,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_USER = 'authapp411@gmail.com'   # config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 
