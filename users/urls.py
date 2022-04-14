42b2aaf5267cea867f1baea69e8851d4f6856dcf
denakwasi <denakwasi@gmail.com>
2022-04-14 17:31:15 +0000

    Refresh Token implemented

--- users/urls.py
+++ users/urls.py
@@ -6,8 +6,27 @@ from drf_yasg import openapi
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+# 
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+from rest_framework.response import Response
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+User = get_user_model()
 
+@api_view(['GET'])
+@permission_classes([AllowAny])
+def get_tokens_for_user(request):
 
+    # find the user base in params
+    user = User.objects.first()
+
+    refresh = RefreshToken.for_user(user)
+
+    return Response({ 
+       'refresh': str(refresh),
+       'access': str(refresh.access_token),
+    })
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -23,15 +42,17 @@ schema_view = get_schema_view(
 )
 
 
+
 urlpatterns = [
    path('admin/', admin.site.urls),
    path('auth/', include('authentication.urls')),
    path('auth/', include('djoser.urls.jwt')),
    path('auth/token/', TokenObtainPairView.as_view(), name='token'),
    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+   # 
+   path('auth/refresh/', get_tokens_for_user, name='refresh'),
 
-
-    # Swagger docs
+   # Swagger docs
    path('swagger/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
    path('auth/docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
