a7a3f3e6c772658bf697e63bf92b829c6d80db7d
denakwasi <denakwasi@gmail.com>
2022-04-16 12:27:46 +0000

    Refresh Token implemented

--- users/settings.py
+++ users/settings.py
@@ -80,8 +80,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=4),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=3),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=15),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
 }
